84752a9c67fc59034a82ff7ecc8e519fd8bf8e2d
linhbuitrong <21130420@st.hcmuaf.edu.vn>
2026-05-17 09:00:00 +0700

    Fix lỗi cú pháp SQL tạo bảng
    - Nãy chạy bị vướng cái ngoặc trong Database.sql, fix lại để chạy mượt script
    - Chỉnh lại xíu cấu hình db

--- ChuyenDeWeb_BackEnd-main/ChuyenDeWeb_BackEnd-main/Database.sql
+++ ChuyenDeWeb_BackEnd-main/ChuyenDeWeb_BackEnd-main/Database.sql
@@ -1,11 +1,10 @@
+DROP TABLE IF EXISTS `image`;
 CREATE TABLE image (
                     id bigint(20) NOT NULL AUTO_INCREMENT,
                     url varchar(255) NOT NULL,
                     public_id varchar(255) NOT NULL,
                     PRIMARY KEY (`id`)
-)
-ALTER TABLE users ADD COLUMN account_type INT DEFAULT 1;
-UPDATE users SET account_type = 0 WHERE username = 'admin';
+);
 -- accountType số 0 là Admin
 -- accountType số 1 là Tài khoản thường
 -- accountType số 2 là tài khoản premium
